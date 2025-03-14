5a5a43ea18e5bc3b2dfc8389d0029293ae94eae5
Ian Shehadeh <ian@shehadeh.net>
2025-03-14 18:13:09 -0400

    test dyadic rational

--- test/DyadicRational.test.ts
+++ test/DyadicRational.test.ts
@@ -1,5 +1,11 @@
 import { expect, test } from "bun:test";
+import { DyadicRational } from "../cgtjs/DyadicRational";
 
-test("2 + 2", () => {
-  expect(2 + 2).toBe(4);
+test("2 + 2 = 4", () => {
+  DyadicRational.from(2).add(DyadicRational.from(2));
+  expect(DyadicRational.from(2).add(DyadicRational.from(2)).toString()).toBe("4");
+});
+
+test("1/2 + 1/8 = 5/8", () => {
+  expect(new DyadicRational(1, 1n).add(new DyadicRational(1, 3n)).toString()).toBe("5/8");
 });
