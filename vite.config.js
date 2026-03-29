43772dabf1c75c4c0fd307106ee019eca46891dc
Ian Shehadeh <ian@shehadeh.net>
2026-03-29 19:57:46 -0400

    add vitest

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+/// <reference types="vitest/config" />
+
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
@@ -6,6 +8,14 @@ import dts from 'vite-plugin-dts';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const sourceRoot = resolve(__dirname, 'cgtjs');
 export default defineConfig({
+  test: {
+    include: ['test/**/*.test.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'lcov'],
+      include: ['cgtjs/**/*.ts'],
+    },
+  },
   build: {
     lib: {
       entry: resolve(sourceRoot, 'index.ts'),
