ebea2d1d0298c8c236996fb8145ed9f4836f4eb6
Ian Shehadeh <ian@shehadeh.net>
2026-04-24 17:47:19 -0400

    implement konane

--- cgtjs/index.ts
+++ cgtjs/index.ts
@@ -1,5 +1,6 @@
 export * from './Board.ts';
 export * from './CanonicalForm.ts';
 export * from './game/Blokus.ts';
+export * from './game/Konane.ts';
 export * from './MoveSet.ts';
 export * from './NumberUpStar.ts';
